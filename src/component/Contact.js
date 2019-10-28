71fc618411f8e64989b4fe519a0db80f24d3608e
Fg-35 <fabiengiboire17@gmail.com>
2019-10-28 12:33:19 +0100

    onclick fini

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,20 +2,31 @@ import React from 'react';
 import './chat-app.css';
 
 
-function Contact (props) {
-    return (
-        <div className = "Contact">
-            <img className = "avatar" src={props.avatar} alt={props.name}/>
-            <div>
-                <p className = "name">{props.name}</p>
-                <div className ="status">{props.status}
-                    <p className= "status-text">{props.status ? <p className="status-online"></p> : <p className="status-offline"></p>}</p>
-                    <p className= "status-text">{props.status ? "Online" : "Offline"}</p>                     
-                </div>
-            </div>            
-        </div>
-        
-    );   
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: false,
+        };
+    }
+    render() {
+        return (
+            <div className = "Contact">
+                <img className = "avatar" src={this.props.avatar} alt={this.props.name}/>
+                <div>
+                    <p className = "name">{this.props.name}</p>
+                    <div className ="status" onClick={event => {const newStatus = !this.state.status;
+                        this.setState({ status: newStatus });
+                        }}>{this.state.status} 
+                        <p className= "status-text">{this.state.status ? <p className="status-online"></p> : <p className="status-offline"></p>}</p>
+                        <p className= "status-text">{this.state.status ? "Online" : "Offline"}</p>                     
+                    </div>
+                </div>            
+            </div>
+            
+        );
+    }
+       
 }
 
 export default Contact;
